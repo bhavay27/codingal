62fd54a251a2edc633df356231edc17739675621
bhavay27 <27bhaweshdhawan@gmail.com>
2024-11-30 19:40:08 +0530

    Create data structure 2.py

---
+++ data structure 2.py
@@ -0,0 +1,15 @@
+n_tuple=("lenevo",[2,4,5],"bhavay")
+print(n_tuple[1][1:])
+print(n_tuple[2][1:4])
+a={1,2,4,3,5,5,6,7,7,8,10}
+b={1,5,7,8,9,51,59}
+print("set A =",a)
+print("set B =",b)
+print("a union b is=",a.union(b))
+print("a intersection b is=",a.intersection(b))
+print("a difference b is=",a.difference(b))
+print("b difference a is=",b.difference(a))
+print("a  symmetric difference b is=",a.symmetric_difference(b))
+
+
+
